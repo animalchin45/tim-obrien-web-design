fc1accdeef8c9038994204d3b16d3f5fc8d48b89
Tim O'Brien <timobrien.inc@gmail.com>
2022-02-04 21:34:44 -0500

    fix title bounce

--- src/components/SectionTitle.js
+++ src/components/SectionTitle.js
@@ -4,13 +4,17 @@ import useElementOnScreen from './hooks/useElementOnScreen'
 const SectionTitle = ({ text }) => {
     const [ containerRef, isVisible ] = useElementOnScreen({
         root: null,
-        rootMargin: "200px",
+        rootMargin: "400px",
         threshold: 1.0
     })
 
     return (
         <div className="portfolio__section__title-area">
-            <h3 ref={containerRef} className={isVisible ? 'portfolio__section__title portfolio__section__title--on' : 'portfolio__section__title portfolio__section__title--off'}>{text}</h3>
+            <h3 
+                ref={containerRef} 
+                className={isVisible ? 'portfolio__section__title portfolio__section__title--on' : 'portfolio__section__title portfolio__section__title--off'}>
+                    {text}
+                </h3>
         </div>
     )
 }
